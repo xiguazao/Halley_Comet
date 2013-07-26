8065a0769e34269381086bf680074eaddaaa451a
xiguazao <wei286069122@163.com>
2013-07-27 05:44:44 +0800

    index2.0 相同url 历史记录

--- HalleyComet/bit/def_url.py
+++ HalleyComet/bit/def_url.py
@@ -5,6 +5,8 @@ import hashlib
 def long_to_short(long_url):
     if long_url[:4] != "http":
         long_url = "http://" + long_url
+    if long_url[-1] != "/":
+        long_url = long_url + "/"
 
     db_url = Url.objects.filter(long_url__exact=long_url)
     if db_url:
